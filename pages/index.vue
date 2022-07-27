e00fac353b214ca3dce8dc7d32f4d429b6194463
Sandros94 <sandro.circi@gmail.com>
2022-07-28 00:34:23 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 	<div>
 		<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 			<div class="max-w-screen-sm sm:text-center sm:mx-auto">
-				<h2 class="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">My Directus Blog</h2>
+				<h2 class="mb-4 font-sans text-3xl font-bold tracking-tight text-red sm:text-4xl sm:leading-none">My Directus Blog</h2>
 				<p class="text-base text-gray-700 md:text-lg sm:px-4">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.</p>
 				<hr class="w-full my-8 border-gray-300" />
 			</div>
