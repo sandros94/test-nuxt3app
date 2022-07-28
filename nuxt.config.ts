57da787af06ffe5d61b4ea8223c40f9d5b08d471
Sandros94 <sandro.circi@gmail.com>
2022-07-28 02:00:56 +0200

    ancora testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['nuxt-directus', '@nuxtjs/tailwindcss'],
+    ssr: true,
     tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
+        cssPath: 'assets/css/tailwind.css',
         configPath: 'tailwind.config.js',
         exposeConfig: false,
         injectPosition: 0,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */ 
 module.exports = {
   content: [
     'components/**/*.{vue,js}',
@@ -9,7 +8,6 @@ module.exports = {
     'App.{js,ts,vue}',
     'app.{js,ts,vue}',
   ],
-  presets: [],
   darkMode: 'class', // or 'class'
   theme: {
     screens: {
@@ -19,35 +17,6 @@ module.exports = {
       'xl': '1280px',
       '2xl': '1536px',
     },
-    colors: ({ colors }) => ({
-      inherit: colors.inherit,
-      current: colors.current,
-      transparent: colors.transparent,
-      black: colors.black,
-      white: colors.white,
-      slate: colors.slate,
-      gray: colors.gray,
-      zinc: colors.zinc,
-      neutral: colors.neutral,
-      stone: colors.stone,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      fuchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose,
-    }),
     extend: {},
   },
   plugins: [],
