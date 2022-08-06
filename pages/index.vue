6391b893ca0d037b002e79d33771f78336a18dee
Sandros94 <13056429+Sandros94@users.noreply.github.com>
2022-08-06 02:35:23 +0200

    esempio pagina servita via code-server rev-proxy

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-	<div>
-		<h2>This is a test</h2>
+	<div class="text-center p-12">
+		<h2 class="text-3xl">si<br>può<br>FARE!</h2>
+		<p class="font-mono py-8">Posso indirizzare e certificare [n] sottodomini in automatico senza doverli specificare uno ad uno!<br>Tutti coperti da certificazione TLS con challange DNS (il più sicuro).</p>
 	</div>
 </template>
