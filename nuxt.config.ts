114e75a3d0429f1fd08a879d1f75e47a872dd0aa
Sandros94 <13056429+Sandros94@users.noreply.github.com>
2022-11-22 20:37:49 +0100

    cleaned up initial nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,18 @@
-export default ({
-    buildModules: [
-        //"@nuxtjs/color-mode",
-        //"@headlessui/vue",
-        //"@heroicons/vue",
-        //"@tailwindcss/aspect-ratio",
-        //"@tailwindcss/forms",
-        //"@tailwindcss/typography",
-        "@nuxtjs/tailwindcss"
-    ],
-    modules: ['nuxt-directus'],
-    tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
-        configPath: 'tailwind.config.js',
-        exposeConfig: false,
-        config: {},
-        injectPosition: 0,
-        viewer: true,
-    },
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default({
+    buildModules: ['@nuxtjs/html-validator','@nuxtjs/google-fonts'],
+    modules: ['@nuxtjs/tailwindcss','@nuxt/image-edge',],
     typescript: {
         shim: false
     },
+    css: [
+        '@/assets/css/main.css',
+    ],
+    tailwindcss: {
+        // Options
+    },
     directus: {
-        url: "https://directus.digitool.media",
-    }
-})
+        url: 'https://directus.digitool.media/'
+        // token: ''
+    },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  variants: {
-    backgroundColor: [
-      "dark",
-      "dark-hover",
-      "dark-group-hover",
-      "dark-even",
-      "dark-odd"
-    ],
-    borderColor: ["dark", "dark-focus", "dark-focus-within"],
-    textColor: ["dark", "dark-hover", "dark-active"]
-  },
-  content: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        // sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
 }
