2e00e63392d2482e2e11cab8c6af0ca470044aae
Sandros94 <sandro.circi@digitoolmedia.com>
2023-02-23 04:19:23 +0100

    test: dark mode switch

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,67 @@
 <template>
-	<div>
-		<NuxtWelcome />
+	<div class="container p-6 mx-auto">
+		<!-- Buttons -->
+		<div class="w-full flex flex-wrap justify-between item-center mb-8">
+			<div class="flex flex-wrap gap-4">
+				<ABtn>Primary</ABtn>
+				
+				<ABtn color="secondary">Secondary</ABtn>
+				
+				<ABtn color="success"> Success </ABtn>
+				
+				<ABtn color="info"> Info </ABtn>
+				
+				<ABtn color="warning"> Warning </ABtn>
+				
+				<ABtn color="danger"> Danger </ABtn>
+			</div>
+			<div class="flex flex-wrap gap-4">
+				<ARadio v-model="colorMode.preference" value="system" label="System"/>
+				<ARadio v-model="colorMode.preference" value="light" label="Light"/>
+				<ARadio v-model="colorMode.preference" value="dark" label="Dark"/>
+			</div>
+			<ASwitch v-model="toggle"/>
+		</div>
+
+		<h1>colorMode.value: {{ colorMode.value }}</h1>
+		<h1>colorMode.preference: {{ colorMode.preference }}</h1>
+		<p>Toggle: {{ toggle }}</p>
+		<p>Test: {{ test }}</p>
+		
+		<!-- Cards -->
+		<div class="grid-row sm:grid-cols-2 cards-demo-container">
+			<!-- 👉 Using props -->
+			<ACard title="Card title" subtitle="Chocolate cake tiramisu donut"
+				text="Ice cream sweet pie pie dessert sweet danish. Jelly jelly beans cupcake jelly-o chocolate bonbon chocolate bar." />
+
+			<ACard class="relative" color="danger" variant="fill" title="Card title"
+				subtitle="Chocolate cake tiramisu donut">
+				<div class="a-card-body a-card-spacer">
+					<p class="text-sm">
+						Ice cream sweet pie pie dessert sweet danish. Jelly jelly beans
+						cupcake jelly-o chocolate bonbon chocolate bar.
+					</p>
+				</div>
+			</ACard>
+		</div>
 	</div>
 </template>
+
+<style>
+/* Light mode */
+body {
+	background: #faf9f8;
+}
+
+/* Dark mode */
+.dark body {
+	background: #0f0f0f;
+}
+</style>
+
+<script setup>
+const colorMode = useColorMode()
+let toggle = ref(colorMode.value === "dark" ? true : false)
+colorMode.preference = (toggle === true) ? "dark" : "light"
+let test = (toggle !== true) ? "dark" : "light"
+</script>
