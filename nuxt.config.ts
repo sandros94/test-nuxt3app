0421e9d2b6ba3dd5360e9f1843cab024a3778722
Sandros94 <sandro.circi@digitoolmedia.com>
2023-04-09 12:19:04 +0200

    feat: testing new anu config inside nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,28 @@ export default defineNuxtConfig({
         'github:digitoolmedia/first#stable',
         'nuxt-seo-kit',
     ],
-    css: [
-        '@anu-vue/preset-theme-default/dist/style.css'
-    ],
+    unocss: {
+        uno: true,
+        attributify: true,
+        typography: true,
+        include: [/.*\/anu-vue\.js(.*)?$/, './**/*.vue', './**/*.md'],
+        icons: {
+          cdn: 'https://esm.sh/',
+          scale: 1.2,
+          extraProperties: {
+            display: 'inline-block',
+            'vertical-align': 'middle',
+            // ...
+          },
+        },
+        webFonts: {
+          fonts: {
+            sans: 'DM Sans',
+            serif: 'DM Serif Display',
+            mono: 'DM Mono',
+          },
+        },
+    },
     app: {
         head: {
             title: 'Website',
