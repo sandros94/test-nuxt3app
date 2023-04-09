3482a975f75dcf5a31342851fe38431cbb3fac48
Sandros94 <sandro.circi@digitoolmedia.com>
2023-04-09 12:38:25 +0200

    update: unocss.config.ts

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,4 +1,4 @@
-import { presetAnu, presetIconExtraProperties } from 'anu-vue'
+import { presetAnu } from 'anu-vue'
 import { presetThemeDefault } from '@anu-vue/preset-theme-default'
 import {
     defineConfig,
@@ -12,6 +12,7 @@ import {
   } from 'unocss'
 
 export default defineConfig({
+    include: [/.*\/anu-vue\.js(.*)?$/, './**/*.vue', './**/*.md'],
     shortcuts: [
         ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
         ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
@@ -22,7 +23,11 @@ export default defineConfig({
         presetIcons({
             cdn: 'https://esm.sh/',
             scale: 1.2,
-            extraProperties: presetIconExtraProperties,
+            extraProperties: {
+                display: 'inline-block',
+                'vertical-align': 'middle',
+                // ...
+            },
         }),
         presetTypography(),
         presetWebFonts({
@@ -38,7 +43,6 @@ export default defineConfig({
         // default theme preset
         presetThemeDefault(),
     ],
-    include: [/.*\/anu-vue\.js(.*)?$/, './**/*.vue', './**/*.md'],
     transformers: [
         transformerDirectives(),
         transformerVariantGroup(),
