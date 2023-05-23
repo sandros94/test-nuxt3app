dfa97bd790198241f49d989ebcae9f7d9d4e5cea
Sandros94 <sandro.circi@digitoolmedia.com>
2023-05-23 14:59:56 +0200

    feat: useAsyncData and refresh button

--- pages/links.vue
+++ pages/links.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="w-full h-full">
         <UContainer class="w-full max-w-8xl px-8 prose dark:prose-invert">
+            <UButton label="Refresh" :loading="pending" @click="refreshData"/>
             <ul v-for="link in links" :key="link">
                 <li>
                     <a :href="link.link" target="_blank" rel="noopener noreferrer">{{ link.name }} | {{ link.date  }}</a>
@@ -14,23 +15,25 @@
 </style>
 
 <script setup scoped>
-const { getItems } = useDirectusItems();
-const links = await getItems({
-    collection: "links",
-    params: {
-        fields: [
-            "id",
-            "name",
-            "link",
-            "date",
-            "status"
-        ],
-        filter: {
-            status: "visible",
+const { data: links, pending, refresh } = useAsyncData(async () => {
+    const { getItems } = useDirectusItems()
+    return await getItems({
+        collection: 'links',
+        params: {
+            fields: ['id', 'name', 'link', 'date', 'status'],
+            filter: {
+                status: 'visible',
+            },
+            sort: ['-date'],
         },
-        sort: [
-            "-date",
-        ],
-    }
-});
+    })
+})
+
+const refreshData = () => {
+    refresh()
+}
+
+onNuxtReady(() => {
+    refreshData()
+})
 </script>
